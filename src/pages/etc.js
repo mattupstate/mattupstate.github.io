b6b135f0511c5707b173897d319f73357a6693dc
Matt Wright <matt@nobien.net>
2021-12-16 09:21:42 -0500

    Remove stray float

--- src/pages/etc.js
+++ src/pages/etc.js
@@ -44,7 +44,7 @@ const EtcPage = ({
             </ul>
           </div>
           <div class="sm:w-2/5">
-            <StaticImage src="../images/headshot-lg.jpg" class="float-right border-2" alt="Headshot of Matt"/>  
+            <StaticImage src="../images/headshot-lg.jpg" class="border-2" alt="Headshot of Matt"/>  
           </div>
         </div>
       </div>
