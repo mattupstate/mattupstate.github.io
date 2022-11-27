7c02da455633a736d6034c61ea4728f350c80478
Matt Wright <mdw1980@gmail.com>
2022-11-27 18:52:16 -0500

    Update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,8 +4,9 @@ import { Link } from "gatsby";
 const FooterComponent = () => {
     return <div class="pl-10 pr-10 pt-6 max-w-3xl mx-auto text-center text-xs mb-5 font-mono">
         <hr class="mt-8 mb-3"/>
-        Copyright © Matthew Wright 2021<br/>
-        Built with <Link to="https://www.gatsbyjs.com/">Gatsby</Link>, <Link to="https://tailwindcss.com/">tailwindcss</Link> &amp; <Link to="https://pages.github.com/">GitHub Pages</Link>
+        Copyright © Matthew Wright 2022<br/>
+        Built with <Link to="https://www.gatsbyjs.com/">Gatsby</Link>, <Link to="https://tailwindcss.com/">tailwindcss</Link> &amp; <Link to="https://pages.github.com/">GitHub Pages</Link><br/>
+        Find me on <a href="https://mastodon.social/@mattupstate" rel="me">Mastodon</a>
     </div>
 }
 
